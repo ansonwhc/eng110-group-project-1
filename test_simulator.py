da6b6ef3805a1427ab8aaaf29c6d7048fbe62c1f
Nikhil Jha <njha@spartaglobal.com>
2022-04-25 17:02:12 +0100

    Added test for checking that trainees are properly distributed amongst test centers

--- test_simulator.py
+++ test_simulator.py
@@ -21,8 +21,18 @@ class Simulator_tests(unittest.TestCase):
         self.assertEqual(self.simulator3.get_num_of_open_centers(), 2, "The number of open centers is not correct")
         self.assertEqual(self.simulator8.get_num_of_open_centers(), 5, "The number of open centres is not correct.")
 
-    def test_centres_do_not_exceed_maximum_trainees(self):
-        for centre in self.simulator3.open_centers.values():
+    def test_centers_do_not_exceed_maximum_trainees(self):
+        for centre in self.simulator3.open_centers:
             self.assertLessEqual(centre, 100, "At least one test has more than 100 trainees")
-        for centre in self.simulator8.open_centers.values():
+        for centre in self.simulator8.open_centers:
             self.assertLessEqual(centre, 100, "At least one test has more than 100 trainees")
+
+    def test_add_trainees_to_centers(self):
+        updated_simulator3 = self.simulator3.add_trainees(20)
+        test_center_not_full = False
+        for center in updated_simulator3.open_centers:
+            if test_center_not_full == False:
+                if center < 100:
+                    test_center_not_full = True
+            else:
+                self.assertEqual(center, 0, "Two centers not at full capacity")
