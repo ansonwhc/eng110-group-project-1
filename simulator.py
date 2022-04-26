63eb98a0463bb2abc67a21944ba6b13c4adf17d0
Bilal Khan <b_khan98@bilals-air.home>
2022-04-26 13:37:06 +0100

    bilals changes old

--- simulator.py
+++ simulator.py
@@ -1,13 +1,16 @@
-class Simulator():
-    def __init__(self):
-        pass
+# class Simulator():
+#     def __init__(self, number_of_weeks):
+#         self.weeknumber = number_of_weeks
+#         pass
 
 import random 
-random.seed(53)
+# random.seed(53)
 
 
-# number of trainees generated
-print(random.randrange(0, 101))
-# number of trainees 
-print(random.randrange(0, 51))
+# # number of trainees generated
+# print(random.randrange(0, 101))
+# # number of trainees 
+# print(random.randrange(0, 51))
 
+num = random.seedint(1,2)
+print(num)
