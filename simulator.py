e6d4c5f9d2e3dbd56e471425afee858d8a8fb2e0
Anson Chan <ansonchan100@gmail.com>
2022-04-26 17:30:36 +0100

    Working progress, failed all tests atm

--- simulator.py
+++ simulator.py
@@ -1,3 +1,97 @@
 class Simulator:
-    def __init__(self, duration):
-        self.duration = duration
+    def __init__(self,
+                 duration: str,
+                 file_name: str = None):
+        self.input = duration
+        self.record_list = [0]
+
+        if file_name is None:
+            self.file_name = "record_file"
+
+        if not self.input.isnumeric():
+            self.input = TypeError
+        else:
+            self.input = int(self.input)
+
+        self.result = self.simulate()
+
+        # internal_variables
+        self.open_centers = []
+        self.num_waiting_list = 0
+
+        self.simulation_info = ""
+
+    def simulate(self, inp=None) -> (list, str):
+        if inp is not None:
+            self.input = inp
+
+        self.update_simulation_info()
+        output = []
+
+        for month in range(self.input):
+            # sample a number of trainees from 50 to 100
+            sampled_num_trainees = self.generate_new_trainees()
+
+            # every two months
+            if (month + 1) % 2 == 0:
+                # add new center
+                self.open_centers.append(0)
+
+            self.distribute_trainees()
+
+        output.extend([self.get_num_of_open_centers(),
+                       self.get_num_of_full_centers(),
+                       self.get_num_working_trainees(),
+                       self.get_num_waiting_list()])
+
+        # self.record()
+        return output
+
+    # Anson
+    def update_simulation_info(self):
+        # self.simulation_info = duration, for now
+        # return simulation info
+        pass
+
+    def record(self):
+        # write to a csv file
+        # f"{self.file_name}.csv"
+        pass
+
+    def get_num_of_open_centers(self, inp=None) -> int:
+        if inp is None:
+            inp = self.open_centers
+
+        # work here ...
+        return 0   # to delete
+
+    def get_num_of_full_centers(self, inp=None) -> int:
+        if inp is None:
+            inp = self.open_centers
+        # work here ...
+        return 0    # to delete
+
+    def get_num_working_trainees(self, inp=None) -> int:
+        if inp is None:
+            inp = self.open_centers
+        # work here ...
+        return 0    # to delete
+
+    def get_num_waiting_list(self, inp=None) -> int:
+        if inp is None:
+            inp = self.open_centers
+        # work here ...
+        return 0    # to delete
+
+    def generate_new_trainees(self,
+                              lower_bound=None,
+                              upper_bound=None) -> int:
+        # work here ...
+        return 0   # to delete
+
+    def distribute_trainees(self):
+        # update self.open_centers
+        # work here ...
+        pass
+
+
