9b94da0699050981b010b0b984f86570f35db09e
Anson Chan <ansonchan100@gmail.com>
2022-04-26 18:17:55 +0100

    Updated simulate() method, 7 passed tests. Basically it was running the simulate() method even with (self.input is TypeError), so it resulted in errors.

--- simulator.py
+++ simulator.py
@@ -2,8 +2,8 @@ class Simulator:
     def __init__(self,
                  duration: str,
                  file_name: str = None):
+
         self.input = duration
-        self.record_list = [0]
 
         if file_name is None:
             self.file_name = "record_file"
@@ -13,44 +13,48 @@ class Simulator:
         else:
             self.input = int(self.input)
 
-        self.result, self.simulation_info = self.simulate()
-
         # internal_variables
-        self.open_centers = []
+        self.open_centers = [0]
         self.num_waiting_list = 0
 
-    def simulate(self, inp=None):
+        if self.input is not TypeError:
+            self.result, self.simulation_info = self.simulate()
+
+    def simulate(self, inp=None) -> (list, str):
         if inp is not None:
             self.input = inp
 
-        # simulation_info = self.update_simulation_info()
+        simulation_info = self.update_simulation_info("")
         output = []
-        simulation_info = ""   # temporary placeholder
-        #
-        # for month in range(self.input):
-        #     # sample a number of trainees from 50 to 100
-        #     sampled_num_trainees = self.generate_new_trainees()
-        #
-        #     # every two months
-        #     if (month + 1) % 2 == 0:
-        #         # add new center
-        #         self.open_centers.append(0)
-        #
-        #     self.distribute_trainees()
-        #
-        # output.extend([self.get_num_of_open_centers(),
-        #                self.get_num_of_full_centers(),
-        #                self.get_num_working_trainees(),
-        #                self.get_num_waiting_list()])
 
-        # self.record()
+        for month in range(self.input):
+            # sample a number of trainees from 50 to 100
+            sampled_num_trainees = self.generate_new_trainees()
+
+            # every two months
+            if (month + 1) % 2 == 0:
+                # add new center
+                self.open_centers.append(0)
+
+            self.distribute_trainees()
+
+        output.extend([self.get_num_of_open_centers(),
+                       self.get_num_of_full_centers(),
+                       self.get_num_working_trainees(),
+                       self.get_num_waiting_list()])
+
+        self.record()
         return output, simulation_info
 
     # Anson
-    def update_simulation_info(self, inp):
+    def update_simulation_info(self, inp="") -> str:
         # self.simulation_info = duration, for now
-        # return simulation info
-        pass
+        add_sim_info = f"{inp}"
+        sim_info = f"\
+        Input: {self.input} months\n\
+        {add_sim_info}"
+
+        return sim_info
 
     def record(self):
         # write to a csv file
