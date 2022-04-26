e0c7afd92a95cdb15143643cedb5b325dc9f4b64
samuel-walters <swalters@spartaglobal.com>
2022-04-26 10:08:45 +0100

    change simulator0 to simulator1

--- test_simulator.py
+++ test_simulator.py
@@ -1,11 +1,13 @@
 import unittest
+import random
 from simulator import Simulator
+random.seed(53)
 
 class Simulator_tests(unittest.TestCase):
     
-   
     def setUp(self) -> None:
         self.simulator0 = Simulator(0)
+        self.simulator1 = Simulator(1)
         self.simulator3 = Simulator(3)
         self.simulator8 = Simulator(8)
         self.simulator25 = Simulator(25)
@@ -49,6 +51,10 @@ class Simulator_tests(unittest.TestCase):
             self.assertEqual(self.simulator25.num_waiting_list, 0, "Trainees from the waiting list are not being assigned to available centers.") 
         if self.simulator8.open_centers.count(100) < len(self.simulator8.open_centers):
             self.assertEqual(self.simulator8.num_waiting_list, 0, "Trainees from the waiting list are not being assigned to available centers.")
+
+    def test_simulator0_outputs(self):
+        self.assertEqual(self.simulator1.open_centers[0], 13, "Failed to assign trainees to the open center in a Simulator object with input 0.")
+        self.assertEqual(self.simulator1.num_waiting_list, 65, "Failed to put extra trainees into the waiting list in a Simulator object with input 0.")
 
     def test_centers_accept_up_to_50_trainees(self):
         obj_lst = [self.simulator3, self.simulator8, self.simulator25]
