657c097659bf5fc2cfe44a69a1ba733b652182c9
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-04-27 14:19:38 +0100

    Removed non-essential tests

--- simulator.py
+++ simulator.py
@@ -1,13 +1,5 @@
-class Simulator():
+class Simulator:
     def __init__(self):
         pass
 
-import random 
-random.seed(53)
-
-
-# number of trainees generated
-print(random.randrange(50, 101))
-# number of trainees 
-print(random.randrange(0, 51))
 
