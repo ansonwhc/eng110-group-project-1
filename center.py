10fdb6793b004317b28e42d97cf75c0058d614e1
samuel-walters <swalters@spartaglobal.com>
2022-04-28 10:38:26 +0100

    Modify generate_center so it will always open a new center

--- center.py
+++ center.py
@@ -2,8 +2,7 @@ import random
 
 class Center():
     def __init__(self):
-
-        self.center_dictionary = {"training_hubs": {}, "bootcamps": {}, "tech_center": {}}
+        self.all_centers = []
         self.waiting_list_dictionary = {
         "Java": 0,
         "C#": 0,
@@ -11,34 +10,44 @@ class Center():
         "DevOps": 0,
         "Business": 0
     }
+        self.num_bootcamps = 0
+        self.num_open_training_hubs = 0
+        self.can_open_training_hub = True
+        self.can_open_bootcamp = True
 
+    def centers_available(self):
+
+        if self.num_open_training_hubs == 3:
+            self.can_open_training_hub = False
+        if self.num_bootcamps == 2:
+            self.can_open_bootcamp = False
+        
+        if self.can_open_training_hub == True and self.can_open_bootcamp == True:
+            random_num = random.randrange(1,4)
+        elif self.can_open_training_hub == False and self.can_open_bootcamp == True:
+            random_num = random.randrange(2, 4)
+        elif self.can_open_training_hub == True and self.can_open_bootcamp == False:
+            random_num = random.choice([1, 3])
+        elif self.can_open_training_hub == False and self.can_open_bootcamp == False:
+            random_num = 3
+        
+        return random_num
 
     def generate_center(self):
-        random_num = random.randrange(1,4)
-        # Training hubs
-        print(random_num)
+        random_num = self.centers_available()
+        # (TODO: Type dictionary {int: type_center})
+        # Training hub
         if random_num == 1:
-            # If there are already three training hubs open, do not open another training hub
-            num_open = len([dictionary for dictionary in self.center_dictionary["training_hubs"] if dictionary["open"] == "yes"])
-            if num_open != 3:
-                # Creates a new id for the newly created training hub
-                id = len(self.center_dictionary["training_hubs"])
-                self.center_dictionary["training_hubs"][str(id)] = {"open": "yes", "type": "training_hubs"}
-        # bootcamps
-        if random_num == 2:
-            # If there are already two bootcamps open, do not open another bootcamp
-            num_open = len([dictionary for dictionary in self.center_dictionary["bootcamps"] if dictionary["open"] == "yes"])
-            if num_open != 2:
-                # Creates a new id for the newly created training hub
-                id = len(self.center_dictionary["bootcamps"])
-                self.center_dictionary["bootcamps"][str(id)] = {"open": "yes", "type": "bootcamps"}
-        # tech centers
-        if random_num == 3:
+            self.all_centers.append({"open": "yes", "type": "training_hubs", "trainee": {}})
+        # Bootcamp
+        elif random_num == 2:
+            self.all_centers.append({"open": "yes", "type": "bootcamp", "trainee": {}})
+        # Tech center
+        elif random_num == 3:
             # Determines what course the tech center will be teaching
             course = random.choice(["Java", "C#", "Data", "DevOps", "Business"])
             # Creates a new id for the tech center 
-            id = len(self.center_dictionary["tech_center"])
-            self.center_dictionary["tech_center"][str(id)] = {"open": "yes", "type": "tech_center", "course": course}
+            self.all_centers.append({"open": "yes", "type": "tech_center", "course": course, "trainee": {}})
     
     def push_to_waiting_list(self):
         pass
@@ -54,5 +63,5 @@ if __name__ == "__main__":
 
     obj = Center()
     obj.generate_center()
-    print(obj.center_dictionary)
+    print(obj.all_centers)
 
