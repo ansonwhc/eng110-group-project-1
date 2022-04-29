cbf6f8ca2b17c89f5d1b524d351bdb6668b60aaf
Nikhil Jha <njha@spartaglobal.com>
2022-04-29 17:29:20 +0100

    Added generate_client method to client class

--- client.py
+++ client.py
@@ -1,4 +1,15 @@
+import random
 class Client:
     def __init__(self):
         self.client_list = []
-    
+
+    def generate_client(self):
+        # Generate client as dictionary and then appends it to client_list
+        new_client = {"client_num": len(self.client_list) + 1,
+                      "requirements": (random.randint(15, 30), random.choice(["Java", "C#", "Data", "DevOps", "Business"])),
+                      "num_of_trainees": 0,
+                      "happy": False}
+        self.client_list.append(new_client)
+
+
+client = Client()
