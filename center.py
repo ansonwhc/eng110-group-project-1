02af8a144407bb89567781a1dff6873b194313e2
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2022-05-03 11:52:39 +0100

    Implemented distribute_training_hub
    Co-authored-by: ansonwhc ansonchan100@gmail.com
    Co-authored-by: NikhilJha42 njha@spartaglobal.com

--- center.py
+++ center.py
@@ -1,16 +1,21 @@
 import random
 
+
 class Center():
     def __init__(self):
         self.all_centers = []
         self.distribute_trainees_list = []
         self.waiting_list_dictionary = {
-        "Java": 1,
-        "C#": 2,
-        "Data": 30,
-        "DevOps": 4,
-        "Business": 5
+            "Java": 1,
+            "C#": 2,
+            "Data": 30,
+            "DevOps": 4,
+            "Business": 5
         }
+        self.max_capacity = {
+            "training_hub": 100,
+            "bootcamp": 500,
+            "tech_center": 200}
         self.num_bootcamps = 0
         self.num_open_training_hubs = 0
         self.can_open_training_hub = True
@@ -22,28 +27,39 @@ class Center():
             self.can_open_training_hub = False
         if self.num_bootcamps == 2:
             self.can_open_bootcamp = False
-        
+
         if self.can_open_training_hub == True and self.can_open_bootcamp == True:
-            random_num = random.randrange(1,4)
+            random_num = random.randrange(1, 4)
         elif self.can_open_training_hub == False and self.can_open_bootcamp == True:
             random_num = random.randrange(2, 4)
         elif self.can_open_training_hub == True and self.can_open_bootcamp == False:
             random_num = random.choice([1, 3])
         elif self.can_open_training_hub == False and self.can_open_bootcamp == False:
             random_num = 3
-        
+
         return random_num
 
     def generate_center(self):
         # random_num = self.centers_available()
-        random_num = 3
+        random_num = 1
         # (TODO: Type dictionary {int: type_center})
         # Training hub
         if random_num == 1:
-            self.all_centers.append({"open": "yes", "type": "training_hubs", "trainee": {}})
+            self.all_centers.append({"open": "yes", "type": "training_hub", "trainee": {
+                "Java": 0,
+                "C#": 0,
+                "Data": 0,
+                "DevOps": 0,
+                "Business": 0
+            }})
         # Bootcamp
         elif random_num == 2:
-            self.all_centers.append({"open": "yes", "type": "bootcamp", "trainee": {}})
+            self.all_centers.append({"open": "yes", "type": "bootcamp", "trainee": {"Java": 0,
+                                                                                    "C#": 0,
+                                                                                    "Data": 0,
+                                                                                    "DevOps": 0,
+                                                                                    "Business": 0
+                                                                                    }})
         # Tech center
         elif random_num == 3:
             # Determines what course the tech center will be teaching
@@ -64,10 +80,6 @@ class Center():
         #     "DevOps": 6,
         #     "Business": 10 }
         # try a code to assess if the centers are full or not
-        max_capacity = {
-            "training_hub": 100,
-            "bootcamp": 500,
-            "tech_center": 200}
         # TODO: if center any center is full add the trainees to another center until all centers are full
         # setting conditions as whether the center are open and have numb_trainees lees than their max limit
         each_center_take_in = []
@@ -87,13 +99,13 @@ class Center():
             center_type = center['type']
             available_num = max_capacity[center_type] - sum(center["trainee"].values()) - num_take_in
             if available_num > 0:
-            # Center is still available
+                # Center is still available
                 self.available_num_lg_0()
             elif available_num == 0:
-            # If they're equal the center is full
+                # If they're equal the center is full
                 self.available_num_eq_0()
             else:
-            # For when sampled number of trainees exceeded the center capacity
+                # For when sampled number of trainees exceeded the center capacity
                 self.available_num_ls_0()
 
     def create_distribution_list(self):
@@ -104,15 +116,12 @@ class Center():
             self.distribute_trainees_list.append(random.randrange(0, 51))
 
     def distribute_tech_centers(self):
-        self.create_distribution_list()
         # Iterating through all_centers
         for index, center in enumerate(self.all_centers):
             # How many trainees a center it can take
             random_trainee = self.distribute_trainees_list[index]
-            print(center)
             # Iterating through all courses in the waiting list
             for course in self.waiting_list_dictionary:
-                print(course)
                 # Finding matching courses
                 if course == center['course']:
                     # Grabbing the number of trainees from the matching course
@@ -128,18 +137,26 @@ class Center():
             # If all centers are full return the trainees to the waiting_list
             # randomly generated trainees need to be allocated to the course types of the waiting_list
 
-
-
-
+    def distribute_training_hub(self):
+        self.create_distribution_list()
+        for index, center in enumerate(self.all_centers):
+            random_trainee = self.distribute_trainees_list[index]
+            if sum(center['trainee'].values()) < self.max_capacity['training_hub']:
+                res = sum(self.waiting_list_dictionary.values())
+                accepting = min([min([res, (self.max_capacity['training_hub'] - res)]), random_trainee])
+                for trainee in range(accepting):
+                    choose_from = [k for k, v in self.waiting_list_dictionary.items() if v > 0]
+                    sampled_course = random.choice(choose_from)
+                    center['trainee'][sampled_course] += 1
+                    self.waiting_list_dictionary[sampled_course] -= 1
 
 
 # distribute from the waiting_list
 # (random 0 - 50) -> random = 1 -> 1 =10 %, 4 = 40 % ,6 = 60% -> 100% ->
 
 
-    # while the number of trainees is >= 0
-    # then we would run all of the while codes
-
+# while the number of trainees is >= 0
+# then we would run all of the while codes
 
 
 if __name__ == "__main__":
@@ -153,11 +170,11 @@ if __name__ == "__main__":
 
     obj = Center()
     obj.generate_center()
-    obj.all_centers[0]['type'] == 'tech_center'
-    # print(obj.waiting_list_dictionary)
+    obj.all_centers[0]['type'] == 'training_hub'
+    print("waiting list:",obj.waiting_list_dictionary)
     print(obj.distribute_training_hub())
     center_dict = obj.all_centers[0]
     wait_dict = obj.waiting_list_dictionary
 
-    # print(center_dict['course'])
-    # print(wait_dict)
+    print("centre list:", center_dict['trainee'])
+    print("waiting list:", wait_dict)
